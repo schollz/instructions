254eec31fcaba6b255e8e39ff4a2bcedc3b2d5f2
Zack Scholl <zack.scholl@gmail.com>
2019-07-22 14:50:23 -0600

    fix tests

--- directions_test.go
+++ directions_test.go
@@ -22,7 +22,7 @@ var ts = []URLDirections{
 	},
 	{
 		"https://pinchofyum.com/the-best-soft-chocolate-chip-cookies",
-		[]string{},
+		[]string{"Preheat the oven to 350 degrees. Microwave the butter for about 40 seconds to just barely melt it. It shouldn’t be hot – but it should be almost entirely in liquid form.", "Using a stand mixer or electric beaters, beat the butter with the sugars until creamy. Add the vanilla and the egg; beat on low speed until just incorporated – 10-15 seconds or so (if you beat the egg for too long, the cookies will be stiff).", "Add the flour, baking soda, and salt. Mix until crumbles form. Use your hands to press the crumbles together into a dough. It should form one large ball that is easy to handle (right at the stage between “wet” dough and “dry” dough). Add the chocolate chips and incorporate with your hands.", "Roll the dough into 12 large balls (or 9 for HUGELY awesome cookies) and place on a cookie sheet. Bake for 9-11 minutes until the cookies look puffy and dry and just barely golden. Warning, friends: DO NOT OVERBAKE. This advice is probably written on every cookie recipe everywhere, but this is essential for keeping the cookies soft. Take them out even if they look like they’re not done yet (see picture in the post). They’ll be pale and puffy.", "Let them cool on the pan for a good 30 minutes or so (I mean, okay, eat four or five but then let the rest of them cool). They will sink down and turn into these dense, buttery, soft cookies that are the best in all the land. These should stay soft for many days if kept in an airtight container. I also like to freeze them."},
 	},
 	{
 		"https://www.modernhoney.com/the-best-chocolate-chip-cookies/",
@@ -60,7 +60,7 @@ var ts = []URLDirections{
 
 func TestTable(t *testing.T) {
 	log.SetLevel("info")
-	for _, t0 := range ts[:2] {
+	for _, t0 := range ts[:3] {
 		fileToGet := t0.URL
 		fileToGet = strings.TrimPrefix(fileToGet, "https://")
 		if string(fileToGet[len(fileToGet)-1]) == "/" {
