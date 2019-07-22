edb693cf4484f35cfa32cf82b0962341e50dd65d
Zack Scholl <zack.scholl@gmail.com>
2019-07-22 14:10:22 -0600

    add more tests

--- directions_test.go
+++ directions_test.go
@@ -20,10 +20,46 @@ var ts = []URLDirections{
 		"https://www.bonappetit.com/recipe/bas-best-chocolate-chip-cookies",
 		[]string{},
 	},
+	{
+		"https://pinchofyum.com/the-best-soft-chocolate-chip-cookies",
+		[]string{},
+	},
+	{
+		"https://www.modernhoney.com/the-best-chocolate-chip-cookies/",
+		[]string{},
+	},
+	{
+		"https://laurenslatest.com/actually-perfect-chocolate-chip-cookies/",
+		[]string{},
+	},
+	{
+		"https://www.allrecipes.com/recipe/10813/best-chocolate-chip-cookies/",
+		[]string{},
+	},
+	{
+		"https://joyfoodsunshine.com/the-most-amazing-chocolate-chip-cookies/",
+		[]string{},
+	},
+	{
+		"https://cakebycourtney.com/soft-chewy-chocolate-chip-cookies/",
+		[]string{},
+	},
+	{
+		"https://www.foodnetwork.com/recipes/dave-lieberman/noodle-kugel-recipe-1946564",
+		[]string{},
+	},
+	{
+		"https://cooking.nytimes.com/recipes/12320-apple-pie",
+		[]string{},
+	},
+	{
+		"https://www.bonappetit.com/recipe/bas-best-chocolate-chip-cookies",
+		[]string{},
+	},
 }
 
 func TestTable(t *testing.T) {
-	log.SetLevel("trace")
+	log.SetLevel("error")
 	for _, t0 := range ts {
 		fileToGet := t0.URL
 		fileToGet = strings.TrimPrefix(fileToGet, "https://")
